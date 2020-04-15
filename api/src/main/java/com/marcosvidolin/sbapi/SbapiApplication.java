dcc9900eda741778b57a6794253f6cc546677f3d
Marcos Vidolin <marcosvidolin@gmail.com>
2020-04-15 11:41:52 -0300

    chore: initial commit

---
+++ api/src/main/java/com/marcosvidolin/sbapi/SbapiApplication.java
@@ -0,0 +1,13 @@
+package com.marcosvidolin.sbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbapiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/marcosvidolin/sbapi/SbapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosvidolin.sbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
